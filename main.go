25e2028812d35faf55a0792a313f6bf23aea3cf2
kkm <kubasurdej@gmail.com>
2020-04-27 00:49:51 +0200

    Add function for getting token alghoritm

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -11,4 +14,30 @@ func main() {
 	}
 	token := os.Args[1]
 	println(token)
+}
+
+func parseToken(token string) string {
+	h := &Header{}
+	segments := strings.Split(token, ".")
+
+	if len(segments) != 3 {
+		log.Fatal("Token has less or more than 3 segments. Make sure you are providing a signed JWS")
+	}
+
+	decodedHeaderJson, err := base64.StdEncoding.DecodeString(segments[0])
+	if err != nil {
+		log.Fatal("Error when decoding token header")
+	}
+
+	err = json.Unmarshal(decodedHeaderJson, h)
+	if err != nil {
+		log.Fatal("Error when parsing header JSON")
+	}
+
+	return h.Algorithm
+}
+
+type Header struct {
+	Algorithm string `json:"alg"`
+	Type      string `json:"typ"`
 }
