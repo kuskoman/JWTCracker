2b17ee349b2ab30bdd99feeb725b35b405075b86
kkm <kubasurdej@gmail.com>
2020-05-03 16:30:56 +0200

    Make tests easier to do on other alghoritms

--- hashing/hasher_test.go
+++ hashing/hasher_test.go
@@ -7,13 +7,24 @@ import (
 )
 
 func TestHS256Hasher(t *testing.T) {
-	h := GetHasher("HS256")
-	payload := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ"
+	payload := "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ"
 	secret := "your-256-bit-secret"
-	sign := h.Sign(payload, secret)
-	escapedSign := utils.EscapeNonUrlChars(sign)
-	expectedSign := "SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-	if escapedSign != expectedSign {
-		t.Errorf("HS256 Hasher returned wrong signature. Expected: %s, got %s", expectedSign, escapedSign)
+	var tests []hasherTest
+	hs256 := hasherTest{Alg: "HS256", Header: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9", expectedSign: "SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"}
+	tests = append(tests, hs256)
+	for _, e := range tests {
+		h := GetHasher(e.Alg)
+		body := e.Header + "." + payload
+		sign := h.Sign(body, secret)
+		escapedSign := utils.EscapeNonUrlChars(sign)
+		if escapedSign != e.expectedSign {
+			t.Errorf("%s Hasher returned wrong signature. Expected: %s, got %s", e.Alg, e.expectedSign, escapedSign)
+		}
 	}
+}
+
+type hasherTest struct {
+	Alg          string
+	Header       string
+	expectedSign string
 }
